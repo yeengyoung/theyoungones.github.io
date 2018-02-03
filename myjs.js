5ecc41db1056ec6f9c7c84a48b54b194b8839677
HUI YEENG CHOONG <theyoungones2017@HUIs-MacBook-Pro.local>
2018-02-03 16:39:33 +0800

    myfirstjs

---
+++ myjs.js
@@ -0,0 +1,28 @@
+
+//devlare a varible
+
+var name;
+var age;
+var hungry;
+
+name="fred";//string,
+age=50;//number
+hungry=false; //boolean -not,and,or
+tired= false;
+vr timetoleave= hungry ||tired
+console.log('hungry && tired:' + (hungry&& tired))
+console.log('hungry || tired:' + timetoleave)
+
+console.log('!hungry&&!tired:'+ (!hungry&&!tired))
+
+if (timetoleave ==true) {
+console.log(>>>>"yes")
+}else {console.log(">>>>no")
+
+}
+
+
+//alert('hello' + name);
+
+//str+str=concatenation
+//conole.log('name='+ name);
