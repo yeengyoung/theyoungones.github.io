e74e435b0aec35557f42c64a844d96e14ec71e83
HUI YEENG CHOONG <theyoungones2017@HUIs-MacBook-Pro.local>
2018-02-07 18:26:22 +0800

    3Feb

---
+++ day2a.js
@@ -0,0 +1,35 @@
+var car={
+    make: "honda",
+    drvie:"4W",
+    prince: 100000,
+    validDrviers: ["fred", "wilma",]
+}
+var fred ={
+    name: "fred",
+    email:"fred@gmail.com"}
+
+var wilma ={
+    name :"wilma",
+    email: "wilma@gamil.com"
+
+    }
+var flintstones= [fred, wilma,]
+var i=0
+while (i < flintstones.length){
+    console.log("email:" +flintstone[i]["email"])
+i+=1;
+}
+console.log(flintstones); 
+
+
+
+    //Print out the email addresses of the flintstones.
+    //Task use JS to create of a deck of cards
+
+    
+
+
+
+
+console.log("carmake=", car["make"]);
+console.log(car make=)
